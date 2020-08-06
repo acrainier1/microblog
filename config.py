c1572a17ae020ac95e738fe6a157f801b7e76968
acrainier1 <alex.c.canizales@gmail.com>
2020-08-05 20:56:44 -0400

    added MAIL_USE_SSL = False in config.py

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = False,
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kanjiremastered@gmail.com']
