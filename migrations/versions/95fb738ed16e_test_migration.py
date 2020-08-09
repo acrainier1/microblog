0784d9edbb7dcd9f69f7945e736e2d4ea4fea956
acrainier1 <alex.c.canizales@gmail.com>
2020-08-08 21:49:33 -0400

    fixed db

--- migrations/versions/95fb738ed16e_test_migration.py
+++ migrations/versions/95fb738ed16e_test_migration.py
@@ -1,8 +1,8 @@
-"""rebuild of  app.db
+"""test migration
 
-Revision ID: b35f7082ab58
+Revision ID: 95fb738ed16e
 Revises: 
-Create Date: 2020-08-06 10:58:02.483449
+Create Date: 2020-08-08 21:48:39.978135
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b35f7082ab58'
+revision = '95fb738ed16e'
 down_revision = None
 branch_labels = None
 depends_on = None
