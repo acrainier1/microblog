1b0f6c3409e0e36a624ab9b2db381271e90e7601
acrainier1 <alex.c.canizales@gmail.com>
2020-08-22 23:18:04 -0400

    db tools

--- db_tools.py
+++ db_tools.py
@@ -2,11 +2,10 @@ import psycopg2
 import os
 
 
-# DATABASE_URL=postgres://*****
-DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
 
 def test(msg="db tools running"):
     print(msg, "DATABASE_URL\n", DATABASE_URL)
