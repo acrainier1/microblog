120fd1c8e3dff955f3ff637307ce17e5a075e04d
acrainier1 <alex.c.canizales@gmail.com>
2020-08-22 23:20:46 -0400

    db tools

--- db_tools.py
+++ db_tools.py
@@ -5,10 +5,15 @@ import os
 # DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+cur.execute('SELECT * FROM user')
+one = cur.fetchone()
+all = cur.fetchall()
 
 
 def test(msg="db tools running"):
     print(msg, "DATABASE_URL\n", DATABASE_URL)
+    print(one, all)
 
 
 if __name__ == '__main__':
