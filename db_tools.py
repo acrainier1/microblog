0942bf521e0e909ed076af50f04de4d601b3f305
acrainier1 <alex.c.canizales@gmail.com>
2020-08-24 13:36:23 -0400

    daily update

--- db_tools.py
+++ db_tools.py
@@ -1,9 +1,15 @@
 import psycopg2
 import os
+import csv
+from config import SQLALCHEMY_DATABASE_URI, KANJI_DATA
 
 
 # DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = SQLALCHEMY_DATABASE_URI
+# KANJI_DATA = 'kanjidata.csv'
+
+
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 cur.execute('SELECT * FROM user')
@@ -11,9 +17,20 @@ one = cur.fetchone()
 all = cur.fetchall()
 
 
+with open(KANJI_DATA, 'r') as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row.
+    cur.copy_from(f, 'kanji_data', sep=',')
+conn.commit()
+
+cur.execute('SELECT * FROM kanji_data')
+one_kd = cur.fetchone()
+all_kd = cur.fetchall()
+
 def test(msg="db tools running"):
     print(msg, "DATABASE_URL\n", DATABASE_URL)
     print(one, all)
+    print(one_kd, all_kd)
 
 
 if __name__ == '__main__':
