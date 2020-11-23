d2d82905fbe832a630c345a14e43282e022a2231
acrainier1 <alex.c.canizales@gmail.com>
2020-11-23 10:41:09 -0500

    weekly db update

--- db_tools/db_tools.py
+++ db_tools/db_tools.py
@@ -17,39 +17,37 @@ cur.execute('SELECT * FROM kanji_data')
 response = cur.fetchone()
 if response:
     cur.execute('TRUNCATE TABLE kanji_data')
-    # conn.commit()
 
 
 with open(KANJI_DATA, 'r') as f:
     next(f) # Skip the header row.
     cur.copy_expert(sql="""COPY kanji_data FROM STDIN WITH (FORMAT CSV)""", file=f)
-    # conn.commit()
 
-cur.execute('''
-    UPDATE kanji_data AS A
+# cur.execute('''
+#     UPDATE kanji_data AS A
 
-        SET "Bushu1" = (SELECT B."Kanji" FROM kanji_data B
-                WHERE A."Radical1" <> '' AND B."Meaning1"=A."Radical1" 
-                   OR A."Radical1" <> '' AND B."Meaning2"=A."Radical1" 
-                   OR A."Radical1" <> '' AND B."Meaning3"=A."Radical1"),
+#         SET "Bushu1" = (SELECT B."Kanji" FROM kanji_data B
+#                 WHERE A."Radical1" <> '' AND B."Meaning1"=A."Radical1" 
+#                    OR A."Radical1" <> '' AND B."Meaning2"=A."Radical1" 
+#                    OR A."Radical1" <> '' AND B."Meaning3"=A."Radical1"),
 
-            "Bushu2" = (SELECT B."Kanji" FROM kanji_data B
-                WHERE A."Radical2" <> '' AND B."Meaning1"=A."Radical2" 
-                   OR A."Radical2" <> '' AND B."Meaning2"=A."Radical2" 
-                   OR A."Radical2" <> '' AND B."Meaning3"=A."Radical2"),
+#             "Bushu2" = (SELECT B."Kanji" FROM kanji_data B
+#                 WHERE A."Radical2" <> '' AND B."Meaning1"=A."Radical2" 
+#                    OR A."Radical2" <> '' AND B."Meaning2"=A."Radical2" 
+#                    OR A."Radical2" <> '' AND B."Meaning3"=A."Radical2"),
 
-            "Bushu3" = (SELECT B."Kanji" FROM kanji_data B
-                WHERE A."Radical3" <> '' AND B."Meaning1"=A."Radical3" 
-                   OR A."Radical3" <> '' AND B."Meaning2"=A."Radical3" 
-                   OR A."Radical3" <> '' AND B."Meaning3"=A."Radical3"),
+#             "Bushu3" = (SELECT B."Kanji" FROM kanji_data B
+#                 WHERE A."Radical3" <> '' AND B."Meaning1"=A."Radical3" 
+#                    OR A."Radical3" <> '' AND B."Meaning2"=A."Radical3" 
+#                    OR A."Radical3" <> '' AND B."Meaning3"=A."Radical3"),
 
-            "Bushu4" = (SELECT B."Kanji" FROM kanji_data B
-                WHERE A."Radical4" <> '' AND B."Meaning1"=A."Radical4" 
-                   OR A."Radical4" <> '' AND B."Meaning2"=A."Radical4" 
-                   OR A."Radical4" <> '' AND B."Meaning3"=A."Radical4")
+#             "Bushu4" = (SELECT B."Kanji" FROM kanji_data B
+#                 WHERE A."Radical4" <> '' AND B."Meaning1"=A."Radical4" 
+#                    OR A."Radical4" <> '' AND B."Meaning2"=A."Radical4" 
+#                    OR A."Radical4" <> '' AND B."Meaning3"=A."Radical4")
 
-        WHERE A."Id" > 0 AND  A."Id" < 9000
-''')
+#         WHERE A."Id" > 0 AND  A."Id" < 9000
+# ''')
 conn.commit()
 
 
